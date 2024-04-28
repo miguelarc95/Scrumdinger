12e4b022e3dadc7a12885bf131bd6db80d5456da
Miguel Cardoso <miguelarc95@gmail.com>
2024-04-28 15:46:04 +0100

    Section 5 - Add life cycle events

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -34,6 +34,13 @@ struct MeetingView: View {
         .padding()
         .foregroundColor(scrum.theme.accentColor)
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear() {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear() {
+            scrumTimer.stopScrum()
+        }
     }
 }
 
