e53867f3bb695a92e2f0d73c997ce53d7c14b8f4
Miguel Cardoso <miguelarc95@gmail.com>
2024-05-01 14:47:26 +0100

    Challenge - Extracted sheet from DetailView into new EditScrumSheet

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct DetailView: View {
     @Binding var scrum: DailyScrum
-    @State private var editingScrum = DailyScrum.emptyScrum
     @State private var isPresentingEditView = false
     
     var body: some View {
@@ -46,27 +45,12 @@ struct DetailView: View {
         .toolbar {
             Button("Edit") {
                 isPresentingEditView = true
-                editingScrum = scrum
             }
         }
         .sheet(isPresented: $isPresentingEditView) {
-            NavigationStack {
-                DetailEditView(scrum: $editingScrum)
-                    .navigationTitle(scrum.title)
-                    .toolbar {
-                        ToolbarItem(placement: .cancellationAction) {
-                            Button("Cancel") {
-                                isPresentingEditView = false
-                            }
-                        }
-                        ToolbarItem(placement: .confirmationAction) {
-                            Button("Done") {
-                                isPresentingEditView = false
-                                scrum = editingScrum
-                            }
-                        }
-                    }
-            }
+            EditScrumSheet(
+                scrum: $scrum,
+                isPresentingEditView: $isPresentingEditView)
         }
     }
 }
